fe0029e8f7adb91a4343b15a3eb188455e8ae29f
Kashyap Kadam <kashyap.kadam@choicehotels.com>
2016-11-13 21:09:31 -0700

    Exploratory Data Analysis Assignment 1

---
+++ plot2.R
@@ -0,0 +1,11 @@
+
+
+data <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?")
+subset <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+datetime <-strptime(paste(subset$Date, subset$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+subset <- cbind(datetime, subset)
+plot(subset$datetime, subset$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.copy(png, file="plot2.png", width=480, height=480)
+dev.off()
+cat("Plot2.png has been saved in", getwd())
+
---
+++ plot3.R
@@ -0,0 +1,14 @@
+data <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?")
+subset <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+datetime <-strptime(paste(subset$Date, subset$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+subset <- cbind(datetime, subset)
+plot(subset$datetime, subset$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+points(subset$datetime, subset$Sub_metering_2, type = "l", xlab = "", ylab = "Energy sub metering", 
+    col = "red")
+points(subset$datetime, subset$Sub_metering_3, type = "l", xlab = "", ylab = "Energy sub metering", 
+    col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", 
+    "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file="plot3.png", width=480, height=480)
+dev.off()
+cat("Plot3.png has been saved in", getwd())
